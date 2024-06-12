ca19a4bb498fd4a8a4df1b3fce76a19fa09728a2
Deadx34 <s22010250@ousl.lk>
2024-06-12 22:35:14 +0530

    new version

--- app/src/main/java/com/isuru/mixit/Sign_in.java
+++ app/src/main/java/com/isuru/mixit/Sign_in.java
@@ -3,6 +3,8 @@ package com.isuru.mixit;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,6 +31,7 @@ public class Sign_in extends AppCompatActivity {
         getEmail = findViewById(R.id.getEmail);
         getPassword = findViewById(R.id.getPassword);
         btnSignIn = findViewById(R.id.btnSignIn);
+        signUpRedirectText = findViewById(R.id.signUpRedirectText); // Initialize the TextView for sign-up redirection
         dbHelper = new DataBaseHelper(this);
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +39,12 @@ public class Sign_in extends AppCompatActivity {
             public void onClick(View view) {
                 String email = getEmail.getText().toString();
                 String password = getPassword.getText().toString();
+
+                // Email validation
+                if (!isValidEmail(email)) {
+                    Toast.makeText(Sign_in.this, "Invalid email format", Toast.LENGTH_SHORT).show();
+                    return; // Exit the method if the email is invalid
+                }
 
                 if (dbHelper.checkUser(email, password)) {
                     Toast.makeText(Sign_in.this, "Login Successful", Toast.LENGTH_SHORT).show();
@@ -54,5 +63,9 @@ public class Sign_in extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
 }
